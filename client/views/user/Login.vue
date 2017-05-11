f92daa4023e3b754e12b0b9e2fe7819f0f80ab5b
sunshumin <ssmin3@163.com>
2017-05-11 20:05:39 +0800

    登陆页到其他页面的跳转

--- client/views/user/Login.vue
+++ client/views/user/Login.vue
@@ -1,5 +1,36 @@
 <template>
   <div>
-    这是登陆页面
+    <el-form :inline="true" :model="userInfor">
+      <el-form-item>
+        <el-input v-model="userInfor.phone" placeholder="联系方式"></el-input>
+      </el-form-item>
+      <el-form-item prop="pass">
+        <el-input type="password" v-model="userInfor.psd" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">登陆</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+<script>
+  export default {
+    data() {
+      return {
+        userInfor: {
+          phone: '',
+          psd: ''
+        }
+      }
+    },
+    methods: {
+      login() {
+        console.log('submit!', this.userInfor.phone, this.userInfor.psd);
+        this.$router.push({
+          path: '/home'
+        })
+      }
+    }
+  }
+
+</script>
